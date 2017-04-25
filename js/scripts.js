393ec92448da30f537043144a17b7202b25edbd5
supimon <supimon@gmail.com>
2017-04-25 15:53:11 +0530

    menu section completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 /**
  * Created by 27 on 24/04/2017.
  */
+
+$(document).ready(function(){
+    // hamburger menu animation
+    $('#nav-icon1').click(function(){ $(this).toggleClass('open'); });
+});
